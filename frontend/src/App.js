f05bfa6d1de189f85f05365ac2fb160274059e9a
LelixAvengarde <talonaxe69@gmail.com>
2023-11-24 00:58:20 -0500

    add dark mode clean css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 // App.js
-import React from 'react';
+import React,{useState} from 'react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import DeviceList from './components/DeviceList';
 import GraphSection from './components/GraphSection';
 import NetworkMap from './components/NetworkMap';
 import './App.css'; // For general styles
+import DarkModeToggle from './components/DarkModeToggle';
 
 function App() {
   return (
     <div className="app">
+<DarkModeToggle className="dark-mode-button"/>
       <Sidebar />
       <Header />
       <DeviceList />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 function Header() {
     return (
       <header className="header-info">
+          
         {/* Header content such as Devices Online/Offline, IP Addresses, etc. */}
       </header>
     );
