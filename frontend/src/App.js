ff37a524fc37eadabfee57365ed47390b032f995
James Gonsalves <jgon0201@gmail.com>
2023-12-10 17:37:25 -0500

    Removing unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,31 +8,6 @@ import GraphA from './components/GraphA'; // Import GraphA
 import GraphB from './components/GraphB'; // Import GraphB
 
 function App() {
-  // usestate for setting a javascript
-  // object for storing and using data
-  const [data, setdata] = useState({
-    name: "",
-    age: null,
-    date: "",
-    programming: "",
-  });
-
-  // Using useEffect for single rendering
-  useEffect(() => {
-    // Using fetch to fetch the api from 
-    // flask server it will be redirected to proxy
-    fetch("/data").then((res) =>
-      res.json().then((data) => {
-        // Setting a data from api
-        setdata({
-          name: data.Name,
-          age: data.Age,
-          date: data.Date,
-          programming: data.programming,
-        });
-      })
-    );
-  }, []);
 
   return (
     <div className="app">
